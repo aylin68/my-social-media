1e8523a4ba725bcf5919c4971fd7805e7bf0aad9
Ailin <aylin.mrLarki@gmail.com>
2022-11-12 22:26:36 +0100

    initialize the frontend application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import { useContext, useEffect } from "react";
+import Login from "./pages/login/Login";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { AuthContext } from "./context/AuthContext";
+import { ProtectedRoute } from "./ProtectedRoute.js";
+
+function App() {
+  const { dispatch } = useContext(AuthContext);
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("userToken");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      // setUser(foundUser);
+      dispatch({ type: "LOGIN_SUCCESS", payload: foundUser.data });
+      // console.log("you are logged in");
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile/:username"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
